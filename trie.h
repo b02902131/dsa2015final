34b1e4770dec7f29dd12c82e8884ded70fa9774e
Bigyo <Bigyo@BigyodeMacBook-Pro.local>
2015-06-01 18:15:57 +0800

    add remove function in trie

--- trie.h
+++ trie.h
@@ -12,9 +12,8 @@ class Trie{
 	private:
 		struct node
 		{
-			bool is_end;
-			int key;
-			int prefix_count;
+			int key;		//if it's not end, key = -1, otherwise key = the word's key
+			//int prefix_count;
 			struct node * child[ALPHABET_SIZE];
 		}*head;
 		int char2index(char c){
@@ -23,35 +22,54 @@ class Trie{
 			if(((int)c - (int)'a') >= 0 && ((int)c - (int)'a') <= 25)return (int)c - (int)'a' + 36;
 			return -1;
 		}
+		void init_node(struct node * n){
+			n->key = -1;
+			//n->prefix_count = 0;
+		}
 	public:
 		void init(){
 			head = new node();
-			head->prefix_count = 0;
-			head->is_end = false;
-			head->key = -1;
+			//head->prefix_count = 0;
+			head->key = -1;		//initialize
 		}
-		void insert(string word){
+		void insert(string word, int key){		//insert a word, with a key
 			node * current = head;
-			current-> prefix_count++;
+			//current-> prefix_count++;
 			for (int i=0;i<word.length();++i){
 				int letter = char2index( word[i] );	//TODO: here should modify to 62 character
-				if((current -> child[letter]) == NULL)
+				if((current -> child[letter]) == NULL){
 					current->child[letter] = new node();
-				current->child[letter]->prefix_count++;
+					init_node(current->child[letter]);
+				}
+				// current->child[letter]->prefix_count++;
 				current = current -> child[letter];
 			}
-			current->is_end = true;
+			current->key = key;
 		}
-		//TODO: add delete function
-
 		//TODO: now returns bool, i will change it to return some data
-		bool search (string word){
+		int search (string word){
 			node * current = head;
 			for(int i=0; i< word.length(); ++i){
-				if(current->child[char2index(word[i])]==NULL) return false;
+				if(current->child[char2index(word[i])]==NULL) return -1;
 				current = current -> child[char2index(word[i])];
 			}
-			return current->is_end;
+			return current->key;
+		}
+		//TODO: add remove function
+		int remove(string word){
+			//find position and key
+			node * current = head;
+			for(int i=0; i< word.length(); ++i){
+				if(current->child[char2index(word[i])]==NULL) return -1;
+				current = current -> child[char2index(word[i])];
+			}
+			//if key = -1 , return -1
+			int key = current->key;
+			if(key==-1) return -1;
+			//set the position: key = -1;
+			current->key=-1;
+			//return the key
+			return key;
 		}
 		int words_with_prefix(string prefix){
 			return -1;
--- try_trie.cpp
+++ try_trie.cpp
@@ -7,10 +7,19 @@
 int main(){
 	Trie trie;
 	trie.init();
-	trie.insert("Alan9");
+	trie.insert("Alan9",2);
 
-	cout << trie.search("alan") << endl;
+	trie.insert("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ",999);
+	cout << trie.search("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ") << endl;
+	
 	cout << trie.search("Alan9") << endl;
-	cout << trie.search("bob") << endl;
+	trie.insert("alan",3);
+	cout << trie.search("ala") << endl;
+	cout << trie.search("alan") << endl;
+	cout << trie.search("alan2") << endl;
+	cout << trie.remove("Alan9") << endl;
+	cout << trie.search("Alan9") << endl;
+	trie.insert("Alan9",2);
+	cout << trie.search("Alan9") << endl;
 	return 0;
 }
